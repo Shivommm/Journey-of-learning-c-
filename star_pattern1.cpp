a61aeb862b31a2cc694a8b1e37f0a860c71595dc
shivom <shivomchauhan142@gmail.com>
2025-07-19 19:32:42 +0530

    star pattern

---
+++ star_pattern1.cpp
@@ -0,0 +1,29 @@
+// star pattern1
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter number of rows: ";
+    cin >> n;
+    int k;
+    cout << "Enter number of coloumns: ";
+    cin >> k;
+    int i, j;
+
+    for (i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= k; j++)
+        {
+            if (j <= i)
+            {
+                cout << "*";
+            }
+            else
+            {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+}
